5f89986205cd2008546e2b0226d5b134aec408c3
Miisuha <tungtranthanh14@gmail.com>
2025-11-05 11:41:04 +0700

    04: authentication UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+  console.log("Rendering SignIn Page");
+  return <SignInView />
+};
+
+export default Page;
+
+// http://localhost:3000/sign-in
