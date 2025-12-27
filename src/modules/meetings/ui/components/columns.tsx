6667738f0f5cad857090a8c390dee3b20ebf8f38
Miisuha <tungtranthanh14@gmail.com>
2025-12-27 08:27:23 +0700

    25: Completed State

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -13,20 +13,13 @@ import {
   LoaderIcon
 } from 'lucide-react';
 import { Badge } from "@/components/ui/badge"
-import humanizeDuration from "humanize-duration"
 import { format } from "date-fns"
-import { cn } from "@/lib/utils";
+import { cn, formatDuration } from "@/lib/utils";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-function formatDuration(seconds: number) {
-  return humanizeDuration(seconds * 1000, {
-    largest: 1,
-    round: true,
-    units: ["h", "m", "s"]
-  })
-}
+
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -18,6 +18,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import { CompletedState } from "../components/completed-state";
 
 interface Props {
     meetingId: string
@@ -81,7 +82,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
                 />
                 {isCancelled && <CancelledState/>}
                 {isProcessing && <ProcessingState/>}
-                {isCompleted && <div>Completed</div>}
+                {isCompleted && <CompletedState data={data}/>}
                 {isUpcoming && (<UpcomingState
                     meetingId={meetingId}
                     onCancelMeeting={() => {}}
